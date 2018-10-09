0656e149b301991b0bcc44b63c4ba4c66ef50135
dmesejo <mesejoleon@gmail.com>
2018-10-08 22:28:42 -0300

    add test for topo function

--- tests/test_topo.py
+++ tests/test_topo.py
@@ -1,13 +1,16 @@
-from topo import Queue
+from topo import Queue, topo
 
 q = Queue([1, 2, 3], priority=False)
 
 assert q.pop() == 1
 assert q.pop() == 2
 
-
 pq = Queue([2, 3, 1, 5, 4], priority=True)
 
 assert pq.pop() == 1
 assert pq.pop() == 2
 assert pq.pop() == 3
+
+g = {1: [2], 2: []}
+
+assert [1, 2] == topo(g.keys(), g.get)
